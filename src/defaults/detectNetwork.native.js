acc01ef77d39fc578d28b12f1257530d8faf0bd3
Kenny Meyer <kenny@kennymeyer.net>
2020-01-03 11:20:17 -0300

    Revert "Fix version comparison"
    This reverts commit 52f9d903b3a87378b8f2923c3e87e79c4522b8ae.

--- src/defaults/detectNetwork.native.js
+++ src/defaults/detectNetwork.native.js
@@ -2,9 +2,9 @@
 import VersionNumber from 'react-native-version-number'
 import compareVersions from 'compare-versions'
 let NetInfo
-if (compareVersions('2.4.13', VersionNumber.appVersion, '>=')) {
+if (compareVersions(VersionNumber.appVersion, '2.4.13', '>=')) {
   NetInfo = require('@react-native-community/netinfo').default; // eslint-disable-line
-} else if (compareVersions('2.0.7', VersionNumber.appVersion, '>=')) {
+} else if (compareVersions(VersionNumber.appVersion, '2.0.7', '>=')) {
   NetInfo = require('@react-native-community/netinfo-4.1.2').default; // eslint-disable-line
 } else {
   NetInfo = require('rn-legacy-59').NetInfo; // eslint-disable-line
